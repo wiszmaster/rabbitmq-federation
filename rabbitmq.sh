a3e62f39c48f95b70be9fa7567baf5791b8092df
kristiandelay <kristiandelay@gmail.com>
2013-07-13 03:44:51 -0400

    start setup for CentOS

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,24 +1,76 @@
+#!/bin/bash
+
+# RabbitMQ Installation script for CentOS and Debian based distros
+
+# Identify Linux Distribution
+if [ -f /etc/debian_version ] ; then
+    DIST="DEBIAN"
+elif [ -f /etc/redhat-release ] ; then
+    DIST="CENTOS"
+else
+    echo ""
+    echo "This Installer should be run on a CentOS or a Debian based system"
+    echo ""
+    exit 1
+fi
+
+function initDebian()
+{
+   echo "Installing RabbitMQ Broker Debian Distro"
+   
 cat >> /etc/apt/sources.list <<EOT
 deb http://www.rabbitmq.com/debian/ testing main
 EOT
 
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
+	wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+	apt-key add rabbitmq-signing-key-public.asc
 
-apt-get update
+	apt-get -y update
 
-apt-get install -q -y screen htop vim curl wget
-apt-get install -q -y rabbitmq-server
+	apt-get install -q -y screen htop vim curl wget
+	apt-get install -q -y rabbitmq-server
 
-# RabbitMQ Plugins
-service rabbitmq-server stop
-rabbitmq-plugins enable rabbitmq_management
-rabbitmq-plugins enable rabbitmq_jsonrpc
-rabbitmq-plugins enable rabbitmq_federation
-rabbitmq-plugins enable rabbitmq_federation_management
-rabbitmq-plugins enable rabbitmq_mqtt
-service rabbitmq-server start
+	# RabbitMQ Plugins
+	service rabbitmq-server stop
+	rabbitmq-plugins enable rabbitmq_management
+	rabbitmq-plugins enable rabbitmq_jsonrpc
+	rabbitmq-plugins enable rabbitmq_federation
+	rabbitmq-plugins enable rabbitmq_federation_management
+	rabbitmq-plugins enable rabbitmq_mqtt
+	service rabbitmq-server start
 
-rabbitmq-plugins list
+	rabbitmq-plugins list
+}
+
+function initCentOS()
+{
+	echo ""
+	echo ""
+	echo "CentOS is not complete at this time, switch to Ubuntu for now sorry."
+	echo ""
+	echo ""
+    exit 1
+	
+   echo "Installing RabbitMQ Broker CentOS Distro"
+	yum -y update
+    VERS=$(cat /etc/redhat-release |cut -d' ' -f4 |cut -d'.' -f1)
+	if [ "$VERS" = "6" ]
+		then
+			echo "version 6"
+		else
+			echo "version 5"	
+	fi
+}
+
+case $DIST in
+	'DEBIAN')
+	initDebian
+;;
+    'CENTOS')
+	initCentOS
+;;
+esac
+
+
 
 
--- worker.sh
+++ worker.sh
@@ -1,7 +1,52 @@
+# RabbitMQ Worker Installation script for CentOS and Debian based distros
 
-apt-get update
+if [ -f /etc/debian_version ] ; then
+    DIST="DEBIAN"
+elif [ -f /etc/redhat-release ] ; then
+    DIST="CENTOS"
+else
+    echo ""
+    echo "This Installer should be run on a CentOS or a Debian based system"
+    echo ""
+    exit 1
+fi
 
-apt-get install -q -y tmux screen htop vim curl wget build-essentials git-core
-apt-get install -q -y ruby1.9.3
+function initDebian()
+{
+   echo "Installing RabbitMQ Worker Debian Distro"
+	apt-get update
 
-gem install bundler
+	apt-get install -q -y tmux screen htop vim curl wget build-essentials git-core
+	apt-get install -q -y ruby1.9.3
+
+	gem install bundler
+}
+
+function initCentOS()
+{
+	echo ""
+	echo ""
+	echo "CentOS is not complete at this time, switch to Ubuntu for now sorry."
+	echo ""
+	echo ""
+    exit 1
+	
+   echo "Installing RabbitMQ Worker CentOS Distro"
+	yum -y update
+    VERS=$(cat /etc/redhat-release |cut -d' ' -f4 |cut -d'.' -f1)
+	if [ "$VERS" = "6" ]
+		then
+			echo "version 6"
+		else
+			echo "version 5"	
+	fi
+}
+
+case $DIST in
+	'DEBIAN')
+	initDebian
+;;
+    'CENTOS')
+	initCentOS
+;;
+esac
